7ee3dfdf879cc3872165113529413524ba58f7f7
Julie Fisher <Julie@Julies-MacBook-Pro.local>
2017-02-25 21:47:47 -0600

    startoftrivia

---
+++ trivia.js
@@ -0,0 +1,12 @@
+
+
+// function- begin the trivia game- questions are not visible until this button is pushed. 
+
+// timer starts- users will have a certain number of seconds to complete the questions, the timer starts when the user clicks on the start triva game
+
+// timer will stop when users hit the "submit answers" button
+
+// score of correct answers will appear as an alert when user clicks the "submit answers" button OR the timer runs out
+
+
+
